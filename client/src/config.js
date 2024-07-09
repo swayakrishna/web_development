082b7307c5f65ec423c104807fe3af2e07e32e51
swayakrishna <165483828+swayakrishna@users.noreply.github.com>
2024-07-09 14:49:59 +0530

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,2 @@
-let BASE_URL = "https://post-it-heroku.herokuapp.com/";
-if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  BASE_URL = "http://localhost:4000/";
-}
-BASE_URL = "http://localhost:4000/";
+let BASE_URL = "https://postgram-eyli.onrender.com/";
 export { BASE_URL };
